8fd16e82f86f5a01ada453a5dfbdafc084e4944e
Brennan Maloney <brennan.maloney02@gmail.com>
2022-11-23 18:52:01 -0500

    update

--- FinalProject.cpp
+++ FinalProject.cpp
@@ -10,25 +10,22 @@ using namespace std;
 int main() {
     // make a map of course codes to names
     fstream nameFile("NameFile.txt", ios::in);
-    std::map<std::string, std::string> dict;
+    std::map<std::string, std::string> m;
     if (nameFile.is_open()) {
         string name, studentID;
         while (getline(nameFile, studentID, ',')) {
             getline(nameFile, name);
-            if (dict.find(studentID) == dict.end()) {
-                dict[studentID] = name;
-            }
+            m[studentID] = name;
         }
+    } else {
+        cout << "Error 'NameFile.txt' is not open" << endl;
+        return 0;
     }
-    //file name not correct
-    else {
-        cout << "Error" << endl;
-    }
-
     // run through CourseFile.txt and output to FormatStudents.txt
     fstream courseFile("CourseFile.txt", ios::in);
     std::ofstream output("FormatStudents.txt");
     if (courseFile.is_open()) {
+        // initialize headers for FormatStudents.txt
         string studentID, courseCode, test1, test2, test3, finalExam;
         output << "Student ID" << setw(20) << "Student Name" << setw(20) << "Course Code" << setw(20) << "Final Grade" << std::endl;
         output << "-------------------------------------------------------------------------" << std::endl;
@@ -38,16 +35,16 @@ int main() {
             getline(courseFile, test2, ',');
             getline(courseFile, test3, ',');
             getline(courseFile, finalExam);
+            //convert inputs from in files to floats
             float finalGrade = ((stof(test1) + stof(test2) + stof(test3)) / 3 * 0.6) + (stof(finalExam) * 0.4);
-            output << studentID << setw(20)<< dict[studentID] << setw(20) << courseCode << setw(20) << finalGrade << std::endl;
+            //output to final grade
+            output << studentID << setw(20)<< m[studentID] << setw(20) << courseCode << setw(20) << finalGrade << std::endl;
         }
         nameFile.close();
         courseFile.close();
         output.close();
-    } 
-    //file name not correct
-    else {
-        cout << "Error" << endl;
+    } else {
+        cout << "Error 'CourseFile.txt' is not open" << endl;
     }
     return 0;
 }
