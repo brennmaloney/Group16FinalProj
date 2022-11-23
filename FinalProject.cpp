25ffcaa1514fd776bbd9ec2c68dbf2e757e1f402
Brennan Maloney <brennan.maloney02@gmail.com>
2022-11-23 18:38:16 -0500

    init

---
+++ FinalProject.cpp
@@ -0,0 +1,54 @@
+
+#include <string>
+#include <fstream>
+#include <stdio.h>
+#include <iostream>
+#include <map>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // make a map of course codes to names
+    fstream nameFile("NameFile.txt", ios::in);
+    std::map<std::string, std::string> dict;
+    if (nameFile.is_open()) {
+        string name, studentID;
+        while (getline(nameFile, studentID, ',')) {
+            getline(nameFile, name);
+            if (dict.find(studentID) == dict.end()) {
+                dict[studentID] = name;
+            }
+        }
+    }
+    //file name not correct
+    else {
+        cout << "Error" << endl;
+    }
+
+    // run through CourseFile.txt and output to FormatStudents.txt
+    fstream courseFile("CourseFile.txt", ios::in);
+    std::ofstream output("FormatStudents.txt");
+    if (courseFile.is_open()) {
+        string studentID, courseCode, test1, test2, test3, finalExam;
+        output << "Student ID" << setw(20) << "Student Name" << setw(20) << "Course Code" << setw(20) << "Final Grade" << std::endl;
+        output << "-------------------------------------------------------------------------" << std::endl;
+        while (getline(courseFile, studentID, ',')) {
+            getline(courseFile, courseCode, ',');
+            getline(courseFile, test1, ',');
+            getline(courseFile, test2, ',');
+            getline(courseFile, test3, ',');
+            getline(courseFile, finalExam);
+            float finalGrade = ((stof(test1) + stof(test2) + stof(test3)) / 3 * 0.6) + (stof(finalExam) * 0.4);
+            output << studentID << setw(20)<< dict[studentID] << setw(20) << courseCode << setw(20) << finalGrade << std::endl;
+        }
+        nameFile.close();
+        courseFile.close();
+        output.close();
+    } 
+    //file name not correct
+    else {
+        cout << "Error" << endl;
+    }
+    return 0;
+}
+
